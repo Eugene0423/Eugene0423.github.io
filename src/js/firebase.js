537003c364191a7c8698ddf24b28964d5fdcc69b
Sally <whiteshin23@gmail.com>
2026-07-15 12:06:15 -0400

    test version: to see whether the firestore is log in

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,6 +1,7 @@
 // src/js/firebase.js
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+import { initializeApp } from
+  "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
 
 import {
   getFirestore,
@@ -17,7 +18,14 @@ import {
   limit,
   setDoc,
   getDoc,
-} from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
+} from
+  "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
+
+import {
+  getAuth,
+  onAuthStateChanged,
+} from
+  "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -30,7 +38,17 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    console.log("Firebase 로그인됨");
+    console.log("UID:", user.uid);
+    console.log("이메일:", user.email);
+  } else {
+    console.log("Firebase 로그인 안 됨");
+  }
+});
 export {
   db,
   collection,
